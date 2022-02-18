69caf93a842f0e1eba116b545b2f086eae73fe5c
Phelps <phelpsmemo@gmail.com>
2022-02-18 07:04:49 -0300

    update headers

--- src/lib/components/InteractiveMap.js
+++ src/lib/components/InteractiveMap.js
@@ -1,7 +1,7 @@
-// This file is part of GEO Feedback React
+// This file is part of GEO Metadata Previewer React
 // Copyright (C) 2022 GEO Secretariat.
 //
-// Geospatial Metadata Viewer is free software; you can redistribute it and/or modify it
+// GEO Metadata Previewer is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
 import _ from "lodash";
